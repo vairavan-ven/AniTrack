c6ecfc4f19e8a38d37a96631730224ee35237aec
vairavan-ven <vairavan.ven@gmail.com>
2024-02-10 18:04:23 +1100

    added website title to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const AppNavbar = () => {
     <Navbar bg='dark' variant='dark' expand='lg'>
       <Container fluid>
         <Navbar.Brand as={Link} to='/'>
-          AniTrack
+          AniTrack: MERN Anime Tracker
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='navbar' />
         <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
